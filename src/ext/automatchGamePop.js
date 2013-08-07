5281473382e1584adaf3232fbca0e24b10edab58
Andrew Iannaccone <andrewiemail@gmail.com>
2013-08-07 13:20:03 +0000

    Automatch over SSL; various other Automatch improvements:
    - automatch disconnect detection and auto-reconnecting
    - notifies server and clears automatch state on game start
    - more event listeners, fewer timed loops
    - general refactoring

--- src/ext/automatchGamePop.js
+++ src/ext/automatchGamePop.js
@@ -3,42 +3,27 @@
 
 var loadAMGamePop = function () {
     "use strict";
-    var AM;
 
-    // Automatch namespace
-    if (!window.hasOwnProperty('AM')) {
-        window.AM = {};
-    }
-    AM = window.AM;
+    // Automatch global namespace
+    var AM = window.AM = (window.AM || {});
 
     AM.appendGamePopup = function (viewport) {
-        viewport.append($(AM.getGamePopHTML()));
-        AM.configureGamePop();
-    };
-
-    // Create UI elements
-    AM.getGamePopHTML = function () {
-        return ['<div id="gamepop" style="visibility:hidden">',
-               '  <h3 style="text-align:center">Creating Automatch Game</h3>',
-               '  ',
-               '  Host: <div id="gamehost"></div><br>',
-               '  Guest: <ul id="gameguests"></ul><br>',
-               '  ',
-               '  <p>',
-               '    Automatch will seat you automatically.',
-               '    Please do not use the Goko UI while ',
-               '    this message is on the screen.',
-               '  </p>',
-               '  ',
-               '  <input type="button" id="abortgame" value="Abort" />',
-               '</div>'].join('\n');
-    };
-
-    // Define UI behavior
-    AM.configureGamePop = function () {
-        // Override Goko's "hide select" elements by default" nonsense
-        $('#gamepop .wtfgoko').css('visibility', 'inherit');
-        $('#gamepop .wtfgoko').css('top', 'auto');
+        viewport.append([
+            '<div id="gamepop" style="visibility:hidden">',
+            '  <h3 style="text-align:center">Creating Automatch Game</h3>',
+            '  ',
+            '  Host: <div id="gamehost"></div><br>',
+            '  Guest: <ul id="gameguests"></ul><br>',
+            '  ',
+            '  <p>',
+            '    Automatch will seat you automatically.',
+            '    Please do not use the Goko UI while ',
+            '    this message is on the screen.',
+            '  </p>',
+            '  ',
+            '  <input type="button" id="abortgame" value="Abort" />',
+            '</div>'
+        ].join('\n'));
 
         // Make this into a lightbox-style dialog
         $('#gamepop').css("position", "absolute");
@@ -52,7 +37,7 @@ var loadAMGamePop = function () {
 
         $('#abortgame').click(function (evt) {
             $('#abortgame').prop('disabled', true);
-            AM.sendMessage('CANCEL_GAME', {matchid: AM.state.game.matchid});
+            AM.abortGame();
             $('#gamepop').css('visibility', 'hidden');
         });
     };
@@ -70,9 +55,13 @@ var loadAMGamePop = function () {
             var hostname = AM.state.game.hostname;
             $('#gamehost').html(hostname);
 
-            // List guests
-            AM.getOppnames(AM.state.game, hostname).map(function (p) {
-                $('#gameguests').append('<li>' + p + '</li>');
+            // List guest names
+            AM.state.game.seeks.map(function (s) {
+                return s.player.pname;
+            }).filter(function (pname) {
+                return pname !== AM.state.game.hostname;
+            }).map(function (pname) {
+                $('#gameguests').append('<li>' + pname + '</li>');
             });
 
             $('#abortgame').prop('disabled', false);
--- src/ext/gokoHelpers.js
+++ src/ext/gokoHelpers.js
@@ -1,152 +1,47 @@
-var defineGokoHelperFunctions = function() {
-    if (typeof AM === 'undefined') {
-       AM = {};
+/*jslint browser: true, devel: true, indent: 4, maxlen: 80, es5: true */
+/*global jQuery, $, FS, WebSocket */
+
+var defineGokoHelperFunctions = function () {
+    "use strict";   // JSLint setting
+
+    var AM = window.AM = (window.AM || {});
+
+    FS.MeetingRoom.prototype.getGamesOwnedBy = function (hostname) {
+        var room = this.helpers.ZoneClassicHelper.currentRoom;
+        return room.get('tableList').models.filter(function (t) {
+            return t.has('owner');
+        }).filter(function (t) {
+            return t.get('owner').get('player').get('playerName') === hostname;
+        });
     };
 
-    AM.getGameOwnedBy = function(hostname) {
-        return mtgRoom.helpers.ZoneClassicHelper.currentTableList.models
-                                                .filter( function(t) {
-            return t.get('owner') && 
-                   (t.get('owner').get('player').get('playerName') === hostname);
-        })[0];
-    }
-
-    // Asynchronously join a game at the first empty seat
-    AM.joinGame = function(tableModel) { 
-        // Get the table index and first open seat
-        opts = {table: tableModel.get('number'),
-                seat: tableModel.get('seats').filter(function(s) {
-                    return s.getOccAddr() === null;
-                })[0].get('number'),
-                ready: true};
-
-        // Join, sit, and tell Goko that we're ready (async)
-        conn.joinAndSit(opts, function () {
-            conn.setReady(opts);
-        });
-    }
-
-    AM.getRoom = function(roomName) {
-        return mtgRoom.roomList.models.filter(function(m) {
-            return m.get('name') == roomName;
+    FS.MeetingRoom.prototype.getRoomByName = function (roomName) {
+        return this.roomList.models.filter(function (m) {
+            return m.get('name') === roomName;
         })[0];
     };
 
-    AM.getAllTables = function() {
-      return mtgRoom.helpers.ZoneClassicHelper
-                    .currentRoom.attributes.tableList.models;
-    }
-
-    AM.getRealTables = function() {
-        return AM.getAllTables().filter(
-            function(m) { return m.get('owner') != null; }
-        );
-    };
-
-    AM.makeGame = function(kingdom) {
-        var testKingdom = kingdom.split(',');
-        var testSettings = {name: "test",
-                            seatsState: [true, true, false, false, false, false],
-                            gameData: {uid: ""},
-                            kingdomCards: testKingdom,
-                            platinumColony: false,
-                            useShelters: false,
-                            ratingType: "casual"};
-        var testOpts = {settings: JSON.stringify(testSettings),
-                        isLock: false,
-                        isRequestJoin: false,
-                        isRequestSit: false,
-                        tableIndex: null};
-        console.log(testOpts);
-        mtgRoom.helpers.ZoneClassicHelper.createTable(testOpts);
-    };
-
-    AM.makeTestgame1 = function() {
-        AM.makeGame('cellar,warehouse,forager,scavenger,scheme,rebuild,tunnel,hamlet,goons,oracle');
-    };
-
-    AM.createGame = function(opps, rsys) {
-
-        // Name the table like "AM for <opp1>, <opp2>, ..., <oppN>"
-        var title = 'AM for ' + opps.join(',');
-
-        // Goko's unfathomably stupid seatState option property
-        var ss = [];
-        for (i = 0; i < 6; i++) { ss.push(i < opps.length + 1); }
-
-        // NOTE: Goko will ignore the kingdom, platinum, and shelters properties.
-        // TODO: Do I need to enter any of these settings at all?
-        // TODO: Is there a helper method for creating a table?
-        var testKingdom = ["gardens", "cellar", "smithy", "village", "councilRoom",
-                           "bureaucrat", "chapel", "workshop", "festival", "market"];
-        var testSettings = {name: title,
-                            seatsState: ss,
-                            gameData: {uid: ""},
-                            kingdomCards: testKingdom,
-                            platinumColony: false,
-                            useShelters: false,
-                            ratingType: rsys};
-        var testOpts = {settings: JSON.stringify(testSettings),
-                        isLock: false,
-                        isRequestJoin: true,
-                        isRequestSit: false,
-                        tableIndex: null};
-
-        // Finally create the table and hide the game announcement 
-        console.log(testOpts);
-        mtgRoom.helpers.ZoneClassicHelper.createTable(testOpts);
-    };
-
-    // Disable/reenable the join request dialog. When disabled, accept the
-    // Automatch game's opponents and reject all others.
-    AM.setAutoaccept = function(enabled, opps) {
-        if (enabled) {
-            // Disable the original dialog. Enable auto-accept.
-
-            // Cache the Goko function that shows a request dialog
-            AM.sbrTemp = mtgRoom.views.ClassicRoomsPermit.showByRequest;
-
-            // Replace it with automatic accept/reject
-            mtgRoom.views.ClassicRoomsPermit.showByRequest = function(req) {
-                var joiner = mtgRoom.playerList
-                                    .findByAddress(req.data.playerAddress)
-                                    .get('playerName');
-                console.log('Received join request from ' + joiner);
-                var jopts = {tag: req, playerAddress: req.data.playerAddress }
-                if (opps.indexOf(joiner) >= 0) {
-                    this.helper.allowPlayerToJoin(jopts);
-                    console.log('... allowed');
-                } else {
-                    this.helper.denyPlayerToJoin(jopts);
-                    console.log('... rejected');
-                }
-            };
-
-        } else {
-
-            // Reenable the original dialog. Disable auto-accept.
-            mtgRoom.views.ClassicRoomsPermit.showByRequest = AM.sbrTemp;
-            delete AM.sbrTemp;
-        } 
-    };
-
-    AM.getHost = function(match) {
-        return match.seeks.filter( function(s) {
-            return s.player.pname == match.hostname;
-        })[0].player;
-    }
-
-    AM.getNames = function(match) {
-        return match.seeks.map(function(s) {
-            return s.player.pname;
+    // Note: Goko will ignore the kingdom for pro games
+    FS.MeetingRoom.prototype.createGame = function (num, rsys, kingdom, title) {
+        var sets, opts, ss;
+        // Create a "seat state" like [true, true, false, false, false, false]
+        ss = [1, 2, 3, 4, 5, 6].map(function (i) {
+            return i <= num;
         });
-    }
-
-    AM.getOppnames = function(match, myName) {
-        return AM.getNames(match).filter(function(p) {
-            return p !== myName;
-        });
-    }
+        sets = {name: (title || "New Game"),
+                seatsState: ss,
+                gameData: {uid: ""},
+                kingdomCards: kingdom,
+                platinumColony: false,
+                useShelters: false,
+                ratingType: rsys};
+        opts = {settings: JSON.stringify(sets),
+                isLock: false,
+                isRequestJoin: false,
+                isRequestSit: false,
+                tableIndex: null};
+        this.getHelper('ZoneClassicHelper').createTable(opts);
+    };
 };
 
 // Execute our code in Goko's JS context by appending it to the document
